5d05afba73f39a3abd0ffc9c05a6c5295b50acab
Timur <tsivakov@yandex.ru>
2019-03-21 23:20:43 +0300

    сделал javascript, а потом его поломалл

---
+++ js/script.js
@@ -0,0 +1,71 @@
+  var link = document.querySelector(".write-us-link");
+  var popup = document.querySelector(".write-us-form");
+  var close = document.querySelector(".close-button");
+  var form = popup.querySelector("form");
+  var login = popup.querySelector("[name=full-name]");
+  var email = popup.querySelector("[name=email]");
+  var isStorageSupport = true;
+  var storage = "";
+
+  try {
+    storage = localStorage.getItem("login");
+  } catch (err) {
+    isStorageSupport = false;
+  }
+
+  link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.add("modal-show");
+    if (storage) {
+      login.value = storage;
+      email.focus();
+    } else {
+      login.focus();
+    }
+  });
+
+  close.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("modal-show");
+  });
+
+  form.addEventListener("submit", function (evt) {
+    if (!login.value || !email.value) {
+      evt.preventDefault();
+      console.log("Необходимо ввести имя и электронную почту");
+    } else {
+      localStorage.setItem("login", login.value);
+    }
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    if (evt.keyCode === 27) {
+      if (popup.classList.contains("modal-show")) {
+         evt.preventDefault();
+         popup.classList.remove("modal-show");
+      }
+    }
+  });
+
+  var mapLink = document.querySelector(".big-map-js");
+  var mapPopup = document.querySelector(".big-map");
+  var mapClose = mapPopup.querySelector(".close-button");
+
+  mapLink.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.add("modal-show");
+  });
+
+  mapClose.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    mapPopup.classList.remove("modal-show");
+  });
+
+  window.addEventListener("keydown", function (evt) {
+    evt.preventDefault();
+    if (evt.keyCode === 27) {
+      if (mapPopup.classList.contains("modal-show")) {
+        mapPopup.classList.remove("modal-show");
+      }
+    }
+  });
